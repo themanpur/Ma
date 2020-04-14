b0f10eda7dc5a81ffaca5f7a1fbfeb24f0f34b91
neword <newordn@gmail.com>
2020-04-14 16:43:32 +0100

    updated CourseAdd design

--- navigation/CourseNavigation.js
+++ navigation/CourseNavigation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import CourseListing from '../views/Course/CourseListing'
 import CourseDetail from '../views/Course/CourseDetail'
+import CourseAdd from '../views/Course/CourseAdd'
 import Header from '../components/Header/Header';
 
 const Stack = createStackNavigator();
@@ -19,6 +20,11 @@ export default function CourseNavigation() {
         <Stack.Screen
           name="CourseDetail"
           component={CourseDetail}
+          options={{ header:()=><Header/>}}
+        />
+        <Stack.Screen
+          name="CourseAdd"
+          component={CourseAdd}
           options={{ header:()=><Header/>}}
         />
        
--- views/Course/CourseListing.js
+++ views/Course/CourseListing.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
 import { StyleSheet,View,FlatList} from 'react-native'
 import CourseItem from '../../components/items/CourseItem'
+import {globalStyles} from '../../assets/styles/globalStyles'
+import { FloatingAction } from 'react-native-floating-action'
+const actions = [
+  {
+    text: 'Ajouter un cours',
+    name: 'bt_new_course',
+    position: 1,
+    color: globalStyles.secondaryColor,
+
+  }
+]
 
 class CourseListing extends Component{
 data = [
@@ -30,9 +41,12 @@ data = [
 ]
     render() {
      
-        return <FlatList showsVerticalScrollIndicator={false} data={this.data} style={styles.container} 
+        return <View style={{flex:1}}>
+        <FlatList showsVerticalScrollIndicator={false} data={this.data} style={styles.container} 
             keyExtractor={(item)=>item.id.toString()} 
         renderItem={({item})=><CourseItem item={item} onPress={()=>this.props.navigation.navigate('CourseNavigation',{screen:'CourseDetail',params:{item}})}/>} />
+        <FloatingAction color={globalStyles.secondaryColor} actions={actions} onPressItem={()=>this.props.navigation.navigate('CourseNavigation',{screen:'CourseAdd'})}/>
+      </View>
     }
 }
 
